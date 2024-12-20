b1a9e9803dce6e63fa8e655f7daea8331f4e71a2
Mehdi Serghini <mehdiaerghini@gmail.com>
2024-12-20 17:28:53 +0100

    done.

--- Module_05/ex00/main.cpp
+++ Module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: meserghi <meserghi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:53:58 by meserghi          #+#    #+#             */
-/*   Updated: 2024/12/14 17:52:47 by meserghi         ###   ########.fr       */
+/*   Updated: 2024/12/20 17:25:25 by meserghi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,16 @@ int main()
 	try
 	{
 		Bureaucrat User1;
-		Bureaucrat King("King", 0);
-		Bureaucrat User2("User2", 150);
+		Bureaucrat King("King", 1);
+		Bureaucrat User2("User2", 1);
+
 		std::cout << User1 << "\n";
-		std::cout << User2 << "\n";
 		std::cout << King << "\n";
+		std::cout << User2 << "\n";
+
+		for (int i = 1; i < 150; i++)
+			User2.decrement();
+		std::cout << User2 << "\n";
 	}
 	catch(std::exception &e)
 	{
